f6857c17e29668d9179bc2150e7afe8ecb8e0acd
Matthias Nannt <matthias@nannt-online.de>
2013-03-05 20:06:45 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UbiqueArt::Application.config.secret_token = '841666140230768971bc87f2aaa6104abf8bd6af21005e093854c4e194eea802d2ea21874acc2e0013dc94afc1b5db38fc9ec1d6755a4cb18f1f2be3c049957a'
