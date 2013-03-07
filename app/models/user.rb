ddb07518a32040bfb76ff5a5fc7014a57457bdd0
Raphael Randschau <nicolai86@me.com>
2013-03-07 13:52:22 +0100

    replace twitter bootstrap with zurb foundation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,15 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+
   has_one :artist_profile
   has_and_belongs_to_many :liked_paintings, :class_name=>'Painting', :join_table => "painting_likes"
-  
+
   has_many :spots
-  
+
   has_and_belongs_to_many :scans, :class_name=>'PaintingAtSpot', :join_table => "scans"
-  
+
+  def admin?
+    return has_role?(:admin)
+  end
 end
