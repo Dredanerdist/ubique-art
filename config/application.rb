2af1ade9d94c0ddaca41885c873d7db9311ec3b2
Niklas Paulsen <npaulsen1990@googlemail.com>
2013-03-10 12:11:47 +0100

    heroku with precompiled assets

--- config/application.rb
+++ config/application.rb
@@ -69,6 +69,9 @@ module UbiqueArt
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # http://guides.rubyonrails.org/asset_pipeline.html
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
