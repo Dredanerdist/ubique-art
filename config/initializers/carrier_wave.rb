89289286140afe97eeffab0fcb7356ec13c3f9ab
Niklas Paulsen <npaulsen1990@googlemail.com>
2013-03-11 16:44:09 +0100

    carrierwave initializer with ENV

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.ftp_host = ENV['FTP_HOST']
+  config.ftp_port = ENV['FTP_PORT']
+  config.ftp_user = ENV['FTP_USER']
+  config.ftp_passwd = ENV['FTP_PASSWORD']
+  config.ftp_folder = ENV['FTP_FOLDER']
+  config.ftp_url = ENV['FTP_URL']
+end
